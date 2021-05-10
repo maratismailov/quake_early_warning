6ad43f0d2f88b6d49abf482282f934a25a25e550
regfe89 <marat.ismailov889@gmail.com>
2021-05-10 15:38:08 +0600

    Update .gitignore, Dockerfile, and 44 more files...

--- install.sh
+++ install.sh
@@ -2,9 +2,5 @@
 `python3 -m venv env`
 source env/bin/activate
 pip install -U pip
-pip install fastapi
-pip install uvicorn
-pip install aiofiles
-pip install jinja2
 pip install telegram-send
 pip freeze -> requirements.txt
